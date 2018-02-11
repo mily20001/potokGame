82c0bf5b96dc1ff64b1739c021540fc83af519c0
mily20001 <mily20001@gmail.com>
2018-02-11 16:25:35 +0100

    feat(server): add file map for server and basic 404 page

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,35 @@
 import http from 'http';
 import fs from 'fs';
 
-const server = http.createServer(function(req, res) {
-    if(req.headers['x-forwarded-protocol'] === 'http') {
-        console.log('upgrading to https');
-        res.writeHead(301, { 'Location': `https://${req.headers['host']}${req.url}`});
-        res.end();
-    }
-    else
-    {
-        // res.writeHead(200, { 'Content-Type': 'text/plain' });
-        // console.log(req.headers['x-forwarded-protocol']);
-        // res.end("hello world!\n");
+import CONFIG from './config';
 
-        const basePath = 'frontend';
+const filesMap = {
+    '/': { file: 'index.html', code: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' } },
+    '/index.html': { code: 301, headers: { Location: '/' } },
+    '/index': { code: 301, headers: { Location: '/' } },
+    '/logo.svg': { file: '', code: 200, headers: { 'Content-Type': 'image/svg+xml' } },
+    '/js/main.js': { file: '', code: 200, headers: { 'Content-Type': 'text/javascript' } },
+    '/css/main.bundle.css': { file: '', code: 200, headers: { 'Content-Type': 'text/css' } },
+};
+
+const server = http.createServer((req, res) => {
+    if (req.headers['x-forwarded-protocol'] === 'http') {
+        console.log('upgrading to https');
+        res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+        res.end();
+    } else {
         console.log('Request:', req.url);
-        if(fs.existsSync(`${basePath}${req.url}`)) {
-            if(req.url === '/logo.svg') {
-                res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
+        if (filesMap[req.url] !== undefined) {
+            res.writeHead(filesMap[req.url].code, filesMap[req.url].headers);
+            if (filesMap[req.url].code === 200) {
+                res.end(fs.readFileSync(`${CONFIG.httpBasePath}${req.url}${filesMap[req.url].file}`));
+            } else if (filesMap[req.url].code === 301) {
+                res.end();
             }
-            res.end(fs.readFileSync(`${basePath}${req.url}`));
-        }
-        else {
-            console.log('Not found:', `${basePath}${req.url}`);
-            res.end();
+        } else {
+            console.log(`Requested file not found: ${req.url}`);
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end(fs.readFileSync(`${CONFIG.httpBasePath}/404.html`));
         }
     }
 });
---
+++ backend/plainConfig.js
@@ -0,0 +1,7 @@
+/* fill and rename to config.js */
+
+export default {
+    dbPassword: '',
+    dbUsername: '',
+    httpBasePath: 'frontend',
+};
