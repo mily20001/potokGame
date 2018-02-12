a3477e0e5a23e9ccd43aa394afa48c36b9e176bb
mily20001 <mily20001@gmail.com>
2018-02-12 20:48:46 +0100

    feat(frontend/login): add login page

--- frontend/Login.jsx
+++ frontend/Login.jsx
@@ -2,21 +2,50 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactRouterPropTypes from 'react-router-prop-types';
 
+import './Login.scss';
+
 export default class Login extends Component {
     componentDidMount() {
         console.log(this.props.history);
     }
     render() {
         return (
-            <div className="container">
-                Hello Login!
+            <div className="container v-centered login-main-container">
+                <div className="jumbotron bg-dark text-light">
+                    <h1 className="text-center">Login</h1><br />
+                    <form className="form-horizontal" action="index.html" method="post">
+                        <div className="form-group row">
+                            <label className="control-label col-sm-2" htmlFor="username">Username:</label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control bg-dark text-white" id="username" name="username" placeholder="Enter username" />
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label className="control-label col-sm-2" htmlFor="pwd">Password:</label>
+                            <div className="col-sm-10">
+                                <input type="password" className="form-control bg-dark text-white" id="pwd" name="password" placeholder="Enter password" />
+                            </div>
+                        </div>
+                        <div id="login-error" className="invisible">
+                            <div className="alert alert-danger" role="alert">
+                                Incorrect username or password
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-sm-offset-2 col-sm-10">
+                                <button type="submit" className="btn btn-outline-light btn-lg">Login</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
+
         );
     }
 }
 
 Login.propTypes = {
-    setUser: PropTypes.function.isRequired,
+    setUser: PropTypes.func.isRequired,
     history: ReactRouterPropTypes.history.isRequired,
 };
 
--- frontend/Main.jsx
+++ frontend/Main.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import {
-    BrowserRouter,
+    Router,
     Route,
     Link,
 } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
 
 import App from './App';
 import Admin from './Admin';
@@ -16,22 +17,30 @@ export default class Main extends Component {
             user: {},
         };
 
+        this.history = createBrowserHistory();
+
+        this.history.push('/login');
+
         this.setUser = this.setUser.bind(this);
     }
 
     setUser(user) {
+        if (user === {}) {
+            this.history.push('/login');
+        }
+
         this.setState({ user });
     }
 
     render() {
         return (
-            <BrowserRouter>
+            <Router history={this.history}>
                 <div className="container fullpage-container">
                     <Route exact path="/" component={App} />
                     <Route path="/admin" render={() => <Admin user={this.state.user} />} />
-                    <Route exact path="/login" render={() => <Login setUser={this.setUser} />} />
+                    <Route exact path="/login" render={() => <Login setUser={this.setUser} history={this.history} />} />
                 </div>
-            </BrowserRouter>
+            </Router>
         );
     }
 }
